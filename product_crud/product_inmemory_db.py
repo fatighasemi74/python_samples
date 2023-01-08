dcf1d3dd4cae8ee16ecd120b275bbb85b291d71d
fati <faateme.ghasemii@gmail.com>
2023-01-08 15:42:01 +0330

    product in memory

---
+++ product_crud/product_inmemory_db.py
@@ -0,0 +1,21 @@
+class ProductOnMemory():
+
+    _product_list = []
+
+    def __int__(self, data:dict):
+        self._data = data
+
+    def add(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def findByID(self, id:int):
+        pass
+
+    def list(self):
+        pass
